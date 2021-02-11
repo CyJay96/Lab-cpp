5a89c28d57ea4c2eaa27bcdfa3ac0f0980493afb
Konstantin Voytko <42088941+CyJay96@users.noreply.github.com>
2021-02-11 13:30:44 +0300

    passed
    changed the condition of the loop while

--- lab_6/lab_6.2/functions.cpp
+++ lab_6/lab_6.2/functions.cpp
@@ -1,19 +1,16 @@
 #include "functions.h"
 
 void create_numbers(ofstream& file, int n) {
-	for (int i = 0; i < n - 1; ++i) {
+	for (int i = 0; i < n; ++i) {
 		file << -50 + rand() % 100 << endl;
 	}
-	file << -50 + rand() % 100 << endl;
 }
 
 void unsigned_sort(ifstream& file1, ofstream& file2, int n) {
 	int* a = new int[n];
 
 	int i = 0;
-	while (!file1.eof()) {
-		file1 >> a[i++];
-	}
+	while (file1 >> a[i++]);
 
 	for (int i = 0; i < n; ++i) {
 		if (a[i] >= 0) {
