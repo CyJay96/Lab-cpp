d5dafbe8823b1bbaf998997a1bd23f5843c98c92
Konstantin Voytko <42088941+CyJay96@users.noreply.github.com>
2021-02-13 21:26:34 +0300

    moving_enum

--- lab_5/lab_5.6/functions.h
+++ lab_5/lab_5.6/functions.h
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+enum WidthValues {
+	widthS = 15,
+	widthT = 15,
+	widthInt = 15,
+	widthK = 15,
+
+	widthLine = 48,
+};
+
 void draw_line(int n);
 double func(double x, double s, double t);
 double sum(double a, double b, int n, double s, double t, double (*func)(double, double, double));
