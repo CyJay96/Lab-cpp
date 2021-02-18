adaf0cd918a6f63bd245749219c6e02c2afd4992
Konstantin Voytko <42088941+CyJay96@users.noreply.github.com>
2021-02-18 11:36:23 +0300

    reinterpret_cast

--- lab_6/lab_6.2/functions.cpp
+++ lab_6/lab_6.2/functions.cpp
@@ -6,6 +6,14 @@ void create_numbers(ofstream& file, int n) {
 	}
 }
 
+void create_numbers_bin(ofstream& file, int n) {
+	for (int i = 0; i < n; ++i) {
+		int number = -50 + rand() % 100;
+		file.write(reinterpret_cast<const char*>(&number), sizeof(number));
+
+	}
+}
+
 void unsigned_sort(ifstream& file1, ofstream& file2, int n) {
 	int* a = new int[n];
 
@@ -21,6 +29,28 @@ void unsigned_sort(ifstream& file1, ofstream& file2, int n) {
 	for (int i = 0; i < n; ++i) {
 		if (a[i] < 0) {
 			file2 << a[i] << endl;
+		}
+	}
+
+	delete[] a;
+}
+
+void unsigned_sort_bin(ifstream& file1, ofstream& file2, int n) {
+	int* a = new int[n];
+
+	int i = 0;
+	while (file1 >> a[i++]);
+	while (file1.read(reinterpret_cast<char*>(&a[i]), sizeof(a[i++])));
+
+	for (int i = 0; i < n; ++i) {
+		if (a[i] >= 0) {
+			file2.write(reinterpret_cast<const char*>(&a[i]), sizeof(a[i]));
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		if (a[i] < 0) {
+			file2.write(reinterpret_cast<const char*>(&a[i]), sizeof(a[i]));
 		}
 	}
 
