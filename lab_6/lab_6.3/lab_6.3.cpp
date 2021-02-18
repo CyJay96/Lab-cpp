ffa3dd6abc774a183ceda6c4fb5d62df1b808a77
Konstantin Voytko <42088941+CyJay96@users.noreply.github.com>
2021-02-18 11:39:19 +0300

    passed

--- lab_6/lab_6.3/lab_6.3.cpp
+++ lab_6/lab_6.3/lab_6.3.cpp
@@ -1,8 +1,6 @@
-﻿/*
+/*
 
-Дата сдачи: ...
-
-Дата последнего редактирования: 28.01.2021
+Дата сдачи: 18.02.2021
 
 Написать программу для работы с базой данных, содержащей записи со сведениями о студентах:
 ФИО, возраст, пол, курс, успеваемость, в которой должны выполняться следующие действия:
@@ -16,90 +14,79 @@
 */
 
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <stdlib.h>
-#include <conio.h>
-
-using namespace std;
-
-struct info {
-	string name;
-	int age;
-	string gender;
-	int course;
-	double mark;
-};
+#include "functions.h"
 
 int main() {
-	setlocale(LC_ALL, "rus");
+	cout << "Enter the number of students:" << endl;
+	cout << "N = ";
+	int n = 0;
+	cin >> n;
+	cout << "Enter the students' details:" << endl;
 
-	const int N = 80;
-	const int MIN_COURSE = 1;
-	const int MAX_COURSE = 4;
 
-	info students[N];
-	info oldest_stud[N];
+	/* TEXT FILE */
+	Info* students = new Info[n];
+	Info* oldest_students = new Info[n];
 
-	ifstream read_f("students.txt");
-	if (!read_f) {
+	ofstream write_file("students.txt");
+	if (!write_file) {
 		cout << "File error" << endl;
 		return 1;
 	}
+	inputData(write_file, students, n);
+	write_file.close();
 
-	int i = 0;
-	while (!read_f.eof()) {
-		string fio = "";
+	int k_oldest = searchOldest(students, oldest_students, n);
 
-		read_f >> students[i].name;
-		read_f >> fio; students[i].name += " " + fio;
-		read_f >> fio; students[i].name += " " + fio;
-		read_f >> students[i].age;
-		read_f >> students[i].gender;
-		read_f >> students[i].course;
-		read_f >> students[i].mark;
-
-		i++;
-	}
-
-	read_f.close();
-
-	int new_pos_oldest = 0;
-	for (int course = MIN_COURSE; course <= MAX_COURSE; ++course) {
-		int pos_oldest = 0;
-		while (students[pos_oldest].course != course) {
-			pos_oldest++;
-		}
-
-		for (int i = pos_oldest + 1; i < N; ++i) {
-			if (students[pos_oldest].age < students[i].age && students[i].course == course) {
-				pos_oldest = i;
-			}
-		}
-
-		for (int i = 0; i < N; ++i) {
-			if (students[i].age == students[pos_oldest].age && students[i].course == course) {
-				oldest_stud[new_pos_oldest++] = students[i];
-			}
-		}
-	}
-
-	ofstream write_f("oldest_students.txt");
-	if (!write_f) {
+	write_file.clear();
+	write_file.open("oldest_students.txt");
+	if (!write_file) {
 		cout << "File error" << endl;
 		return 2;
 	}
+	writeData(write_file, oldest_students, k_oldest);
+	write_file.close();
 
-	for (int i = 0; i < new_pos_oldest; ++i) {
-		write_f << oldest_stud[i].name << " " << oldest_stud[i].age << " " << oldest_stud[i].gender << " "
-			<< oldest_stud[i].course << " " << oldest_stud[i].mark << endl;
+
+	/* BINARY FILE */
+	Info* oldest_students_bin = new Info[n];
+
+	write_file.clear();
+	write_file.open("students.bin", ios::binary);
+	if (!write_file) {
+		cout << "File error" << endl;
+		return 3;
 	}
+	writeData(write_file, students, n);
+	write_file.close();
 
-	write_f.close();
+	k_oldest = searchOldest(students, oldest_students_bin, n);
+
+	write_file.clear();
+	write_file.open("oldest_students.bin", ios::binary);
+	if (!write_file) {
+		cout << "File error" << endl;
+		return 4;
+	}
+	writeDataBin(write_file, oldest_students_bin, k_oldest);
+	write_file.close();
+
+	delete[] students, oldest_students, oldest_students_bin;
 
 	cout << "The text file is ready..." << endl;
-	//cout << "The binary file is ready..." << endl;
+	cout << "The binary file is ready..." << endl;
+
+
+	cout << endl << "Data in text file:" << endl;
+	ifstream read_file("oldest_students.txt");
+	outputData(read_file);
+	read_file.close();
+
+	cout << endl << "Data in binary file:" << endl;
+	read_file.clear();
+	read_file.open("oldest_students.bin", ios::binary);
+	outputDataBin(read_file);
+	read_file.close();
 
 	cout << endl << "Press any key to continue..." << endl;
 	_getch();
