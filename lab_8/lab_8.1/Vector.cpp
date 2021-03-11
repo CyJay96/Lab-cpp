18411c33b9f92ef4132280f3a2684c5b1fd0905d
Konstantin Voytko <42088941+CyJay96@users.noreply.github.com>
2021-03-11 13:33:01 +0300

    add new func

--- lab_8/lab_8.1/Vector.cpp
+++ lab_8/lab_8.1/Vector.cpp
@@ -10,6 +10,7 @@ Vector::Vector() {
 }
 
 Vector::Vector(int size) {
+	this->size < 0 ? this->size = 0 : true;
 	this->setSize(size);
 	this->arr = new int[this->getSize()];
 	for (int i = 0; i < this->getSize(); ++i) {
@@ -32,7 +33,35 @@ Vector::~Vector() {
 	}
 }
 
+istream& operator >> (istream& file, Vector& obj) {
+	while (!file.eof()) {
+		int number = 0;
+		enter(file, number);
+		obj.arr[number]++;
+	}
+
+	return file;
+}
+
+ostream& operator << (ostream& file, const Vector& obj) {
+	for (int i = 0; i < obj.getSize(); ++i) {
+		file << obj.arr[i] << endl;
+	}
+
+	return file;
+}
+
 int& Vector::operator [] (int index) {
+	if (index >= 0 && index < this->getSize()) {
+		return this->arr[index];
+	}
+	else {
+		cout << "Going out of the array" << endl;
+		exit(0);
+	}
+}
+
+int& Vector::operator [] (int index) const {
 	if (index >= 0 && index < this->getSize()) {
 		return this->arr[index];
 	}
@@ -46,12 +75,32 @@ void Vector::setSize(int size) {
 	this->size = size;
 }
 
-int Vector::getSize() {
+int Vector::getSize() const {
 	return this->size;
 }
 
 void Vector::fillRandom(int range) {
 	for (int i = 0; i < this->getSize(); ++i) {
 		this->arr[i] = 1 + rand() % range;
+	}
+}
+
+int createResultSize(const Vector& sort_array, int N) {
+	int sizeRes = 0;
+	for (int i = 0; i < N; ++i) {
+		if (sort_array[i] != 0) {
+			sizeRes++;
+		}
+	}
+
+	return sizeRes;
+}
+
+void createResult(Vector& result, const Vector& sort_array, int N) {
+	int k = 0;
+	for (int i = N - 1; i >= 0; --i) {
+		if (sort_array[i] != 0) {
+			result[k++] = i;
+		}
 	}
 }
