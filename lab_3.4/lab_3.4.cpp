9524b4bf92b1cd0b382962f5e8904dfd23bcdf99
Konstantin Voytko <42088941+CyJay96@users.noreply.github.com>
2021-09-26 15:04:21 +0300

    updated files part 2

---
+++ lab_3.4/lab_3.4.cpp
@@ -0,0 +1,69 @@
+﻿/*
+Дана действительная матрица порядка n × m. Перебор строк/столбцов матрицы
+осуществить с использованием указателей. Обосновать выбор метода сортировки.
+
+Упорядочить столбцы по невозрастанию первых элементов.
+
+a[i][j] = *(*(a + i) + j).
+*/
+
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	srand(time(0));
+
+	int n, m;
+	int a[100][100], b[100][100];
+
+	cout << "Введите порядок матрицы n и m:" << endl;
+
+	cout << "n = ";
+	cin >> n;
+
+	cout << "m = ";
+	cin >> m;
+
+	cout << "Элементы данной матрицы:" << endl;
+
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < n; ++j) {
+			a[i][j] = 10 + (rand() % 90);
+
+			cout << *(*(a + i) + j) << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < m - 1; ++i) {
+		for (int j = 0; j < m - i - 1; ++j) {
+			if (a[0][j] <= a[0][j + 1]) {
+				//сортируем столбцы по первому элементу
+				int* buf = new int[n];
+
+				for (int k = 0; k < n; ++k) {
+					buf[k] = a[k][j];
+					a[k][j] = a[k][j + 1];
+					a[k][j + 1] = buf[k];
+				}
+
+				delete[] buf;
+			}
+		}
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < n; ++j) {
+			cout << *(*(a + i) + j) << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
---
+++ lab_4.2/lab_4.2.cpp
@@ -0,0 +1,54 @@
+﻿/*
+
+Дата сдачи: 02.11.2020
+
+Дата последнего редактирования: 22.01.2021
+
+Выполнить задание, используя C-строки для представления данных.
+
+Написать программу перевода вещественных чисел из шестнадцатеричной
+системы счисления в двоичную.
+
+*/
+
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	const int SIZE = 18;
+	const int N = 2000;
+	const int SIZE_BIN = 5;
+
+	const char HEX[SIZE] = "0123456789ABCDEF.";
+	const char hex[SIZE] = "0123456789abcdef.";
+
+	const char bin[SIZE][SIZE_BIN] = { {"0000"}, {"0001"}, {"0010"}, {"0011"}, {"0100"}, {"0101"}, {"0110"}, {"0111"},
+									   {"1000"}, {"1001"}, {"1010"}, {"1011"}, {"1100"}, {"1101"}, {"1110"}, {"1111"}, {"."} };
+
+	char num16[N] = { 0 };
+	char num2[N] = { 0 };
+
+	cout << "Enter a number in hexadecimal notation: ";
+
+	cin.getline(num16, SIZE);
+
+	for (int i = 0; i < strlen(num16); ++i) {
+		for (int j = 0; j < SIZE; ++j) {
+			if (num16[i] == HEX[j] || num16[i] == hex[j]) {
+				strcat_s(num2, bin[j]);
+				strcat_s(num2, " ");
+				break;
+			}
+		}
+	}
+
+	cout << "This number in binary notation: " << num2 << endl;
+
+	cout << endl << "Press any key to continue..." << endl;
+	_getch();
+
+	return 0;
+}
