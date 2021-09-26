077eaab966a7f2fd324c83540dadfe99ad29e694
Konstantin Voytko <42088941+CyJay96@users.noreply.github.com>
2021-09-26 14:57:00 +0300

    delete lab_7

--- lab_7/lab_7.2/functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <conio.h>
-
-using namespace std;
-
-void input(int* mas1, int** mas2, int n);
-void output(int* mas, int n);
-void output(int** mas, int n);
